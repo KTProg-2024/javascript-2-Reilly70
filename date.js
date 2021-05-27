e41340c3bf7ce3c8af048a2e1a4990decbd6ba89
Reilly Kinz <>
2021-05-27 14:28:57 +0000

    assignment completed

--- date.js
+++ date.js
@@ -3,3 +3,11 @@ function getDate()
 	var dateString = new Date();
 	document.getElementById("date").innerHTML=dateString;
 }
+function green()
+{
+	document.getElementById("date").style.color="green";
+}
+function red()
+{
+	document.getElementById("date").style.color="red";
+}
